0d9b80cf29bedc1c368f6bd4236d2aabb39062bf
Jenna Guest <jenaleigh01@gmail.com>
2024-03-05 02:46:10 +0000

    Created new superuser

--- codestar/settings.py
+++ codestar/settings.py
@@ -27,7 +27,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # SECURITY WARNING: keep the secret key used in production secret!
 SECRET_KEY = os.environ.get("SECRET_KEY")
 
-DEBUG = False
+DEBUG = True
 
 ALLOWED_HOSTS = ['8000-jenaleigh01-djangoblog-yvwblj5cp1z.ws-eu108.gitpod.io', '.herokuapp.com']
 
@@ -90,7 +90,10 @@ DATABASES = {
     dj_database_url.parse(os.environ.get("DATABASE_URL"))
 }
 
-
+CSRF_TRUSTED_ORIGINS = [
+    "https://*.codeanyapp.com",
+    "https://*.herokuapp.com"
+]
 # Password validation
 # https://docs.djangoproject.com/en/4.2/ref/settings/#auth-password-validators
 
