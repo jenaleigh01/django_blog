0fe48319d1db1942b882f47ae2f51d6ced872104
Jenna Guest <jenaleigh01@gmail.com>
2024-03-07 12:00:03 +0000

    Added comments

--- codestar/settings.py
+++ codestar/settings.py
@@ -27,7 +27,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # SECURITY WARNING: keep the secret key used in production secret!
 SECRET_KEY = os.environ.get("SECRET_KEY")
 
-DEBUG = False
+DEBUG =False 
 
 ALLOWED_HOSTS = ['8000-jenaleigh01-djangoblog-tj1f6xza55e.ws-eu108.gitpod.io', '.herokuapp.com']
 
