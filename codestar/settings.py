e0d9c254601caddf4f8550ab4035bc11bd913d60
Jenna Guest <jenaleigh01@gmail.com>
2024-03-08 13:53:46 +0000

    Added url home link

--- codestar/settings.py
+++ codestar/settings.py
@@ -28,7 +28,7 @@ TEMPLATES_DIR = os.path.join(BASE_DIR, 'templates')
 # SECURITY WARNING: keep the secret key used in production secret!
 SECRET_KEY = os.environ.get("SECRET_KEY")
 
-DEBUG = True
+DEBUG = False
 
 ALLOWED_HOSTS = ['8000-jenaleigh01-djangoblog-tj1f6xza55e.ws-eu108.gitpod.io', '.herokuapp.com']
 
