dada4aa42f7e3b8762826d65ade19ab763b90a54
Jenna Guest <jenaleigh01@gmail.com>
2024-03-19 16:37:15 +0000

    Added test files

--- about/tests.py
+++ about/tests.py
@@ -1,3 +1,14 @@
 from django.test import TestCase
+from .forms import CollaborateForm
 
-# Create your tests here.
+
+class TestCollaborateForm(TestCase):
+
+    def test_form_is_valid(self):
+        """ Test for all fields"""
+        form = CollaborateForm({
+            'name': 'John',
+            'email': 'test@test.com',
+            'message': 'Hello!'
+        })
+        self.assertTrue(form.is_valid(), msg="Form is not valid")
